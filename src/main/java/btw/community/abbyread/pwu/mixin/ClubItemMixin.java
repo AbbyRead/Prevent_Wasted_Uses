fccadf3d6d09c38880b42bf821d968911c53ea10
Abigail Read <AbbyReadMail@gmail.com>
2025-10-27 15:15:33 -0700

    Remove inapplicable classes and add weapons

--- src/main/java/btw/community/abbyread/pwu/mixin/ClubItemMixin.java
+++ src/main/java/btw/community/abbyread/pwu/mixin/ClubItemMixin.java
@@ -1,20 +1,17 @@
 package btw.community.abbyread.pwu.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
+import btw.community.abbyread.pwu.util.UsefulnessHelper;
+import btw.item.items.ClubItem;
 import net.minecraft.src.Block;
 import net.minecraft.src.EntityLivingBase;
-import net.minecraft.src.ItemShears;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
-import btw.community.abbyread.pwu.util.UsefulnessHelper;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ItemShears.class)
-public class ItemShearsMixin {
+@Mixin(ClubItem.class)
+public class ClubItemMixin {
 
     @Redirect(
             method = "onBlockDestroyed",
@@ -50,19 +47,4 @@ public class ItemShearsMixin {
             stack.damageItem(damageAmount, usingEntity);
         }
     }
-
-    @Inject(method = "isEfficientVsBlock", at = @At("RETURN"), cancellable = true)
-    private void ar_pwu$efficientOnTallGrass(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
-        if (block == Block.tallGrass) {
-            cir.setReturnValue(true);
-        }
-    }
-
-    @Inject(method = "canHarvestBlock", at = @At("RETURN"), cancellable = true)
-    private void ar_pwu$canHarvestTallGrass(ItemStack stack, World world, Block block, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
-        if (block == Block.tallGrass) {
-            cir.setReturnValue(true);
-        }
-    }
-
 }
